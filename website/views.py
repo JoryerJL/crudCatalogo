2df89c244904df1f797c12c904a06ffe78c5d1bc
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-10-30 17:21:55 -0600

    configurar los urls

--- website/views.py
+++ website/views.py
@@ -1,3 +1,10 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Principal',
+    }
+    return render(request, 'website/index.html')
