dade7dcf2940a91d5943765ba4eb6b80c3d1c676
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-10-31 13:14:22 -0600

    create table dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from productos.models import *
 
 # Create your views here.
 def index_dashboard(request):
+    productos = Producto.objects.all
     context = {
-        'title': 'Dashboard'
+        'title': 'Dashboard',
+        'productos': productos
     }
     return render(request, 'dashboard/index.html', context)
