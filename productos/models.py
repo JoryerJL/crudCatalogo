0457f0e70e98fb07ca83f3dd571d71cab5ac2df2
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-10-31 00:48:37 -0600

    create views y productos, index con catalogo

--- productos/models.py
+++ productos/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=250)
     imagen = models.ImageField()
+    detalle = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
 
     def __str__(self):
--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
 from lib2to3.fixes.fix_input import context
+from productos.models import *
 
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    productos = Producto.objects.all()
     context = {
         'title': 'Principal',
+        'productos': productos
     }
-    return render(request, 'website/index.html')
+    return render(request, 'website/index.html', context)
