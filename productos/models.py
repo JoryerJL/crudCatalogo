5947c3966cf984d205eea25a3cf878c434b30878
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-10-31 16:38:46 -0600

    actualizacion de productos categorias

--- productos/models.py
+++ productos/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=250)
     imagen = models.ImageField()
     detalle = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
 
     def __str__(self):
