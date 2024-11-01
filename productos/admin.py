990c29114cff3ef1cc586122452c9a7b84a6463d
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-01 16:49:11 -0600

    update and add imagenes model

--- productos/admin.py
+++ productos/admin.py
@@ -4,3 +4,4 @@ from productos.models import *
 # Register your models here.
 admin.site.register(Producto)
 admin.site.register(Categoria)
+admin.site.register(Imagenes)
--- productos/models.py
+++ productos/models.py
@@ -7,6 +7,12 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 
+class Imagenes (models.Model):
+    imagen = models.ImageField()
+
+    def __str__(self):
+        return self.imagen.url
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=250)
     imagen = models.ImageField()
@@ -14,6 +20,7 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     activo = models.BooleanField(default=True)
+    galeria = models.ManyToManyField(Imagenes, blank=True, null=True)
 
 
     def __str__(self):
